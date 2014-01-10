45b987278aa4cfc972e152fc469378c53542ab39
francescodavidnero@gmail.com <francescodavidnero@gmail.com@81b2ac94-fbc2-da70-ebc2-4190521568d7>
2014-01-10 12:32:39 +0000

    - Login works - Registration works - Unregistration works - Refactored ejbModule structure

--- trunk/Viaggio-Sogno-EJB/ejbModule/control/UserMgrBean.java
+++ trunk/Viaggio-Sogno-EJB/ejbModule/control/UserMgrBean.java
@@ -1,4 +1,4 @@
-package model;
+package control;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +10,10 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
+import model.Group;
+import model.User;
 import usermanagement.UserMgr;
 import usermanagement.dto.UserDTO;
 
@@ -27,7 +31,7 @@ public class UserMgrBean implements UserMgr {
 
 
 	@Override
-	public void save(UserDTO user) {
+	public void addCustomer(UserDTO user) {
 		User newUser = new User(user);
 		List<Group> groups = new ArrayList<Group>();
 		groups.add(Group.CUSTOMER);
@@ -37,7 +41,27 @@ public class UserMgrBean implements UserMgr {
 
 
 	@Override
-	@RolesAllowed({Group._CUSTOMER,Group._EMPLOYEE})
+	@RolesAllowed({Group._EMPLOYEE})
+	public void addEmployee(UserDTO user) {
+		User newUser = new User(user);
+		List<Group> groups = new ArrayList<Group>();
+		groups.add(Group.EMPLOYEE);
+		newUser.setGroups(groups);
+		em.persist(newUser);
+	}
+
+
+	@Override
+	@RolesAllowed({Group._CUSTOMER})
+	public void updateSelf(UserDTO user) {
+		getPrincipalUser().setEmail(user.getEmail());
+		getPrincipalUser().setPassword(DigestUtils.sha512Hex(user.getPassword()));
+		getPrincipalUser().setFirstName(user.getFirstName());
+		getPrincipalUser().setLastName(user.getLastName());
+	}
+
+	@Override
+	@RolesAllowed({Group._EMPLOYEE})
 	public void update(UserDTO user) {
 		em.merge(new User(user));
 	}
@@ -52,26 +76,41 @@ public class UserMgrBean implements UserMgr {
 
 
 	@Override
+	@RolesAllowed({Group._EMPLOYEE})
+	public void remove(String email) {
+		remove(findByEmail(email));
+	}
+
+
+	private User findByEmail(String email) {
+		return em.createQuery("SELECT t FROM User t where t.email = :email", User.class)
+				.setParameter("email", email).getSingleResult();
+	}
+
+
+	@Override
 	@RolesAllowed({Group._CUSTOMER})
 	public void unregister() {
 		remove(getPrincipalUser());
 	}
 
 
-	public User find(String email) {
-		return em.createQuery("SELECT t FROM User t where t.email = :email", User.class)
-                .setParameter("email", email).getSingleResult();
+	public User find(int ID) {
+		return em.find(User.class, ID);
 	}
+
 
 	public List<User> getAllUsers() {
 		return em.createNamedQuery(User.FIND_ALL, User.class)
 				.getResultList();
 	}
 
-	public void remove(String email) {
-		User user = find(email);
+
+	public void remove(int ID) {
+		User user = find(ID);
 		em.remove(user);
 	}
+
 
 	public void remove(User user) {
 		em.remove(user);
@@ -79,13 +118,19 @@ public class UserMgrBean implements UserMgr {
 
 
 	public User getPrincipalUser() {
-		return find(getPrincipalEmail());
+		return find(getPrincipalID());
+	}
+
+
+	private int getPrincipalID() {
+		return findByEmail(getPrincipalEmail()).getIdUSER();
 	}
 
 
 	public String getPrincipalEmail() {
 		return context.getCallerPrincipal().getName();
 	}
+
 
 	private UserDTO convertToDTO(User user) {
 		UserDTO userDTO = new UserDTO();
