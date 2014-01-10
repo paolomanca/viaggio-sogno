8dad6239a4e5e58be952b754fb9577d4b7702994
francescodavidnero@gmail.com <francescodavidnero@gmail.com@81b2ac94-fbc2-da70-ebc2-4190521568d7>
2014-01-10 12:32:43 +0000

    - Login works - Registration works - Unregistration works - Refactored ejbModule structure

--- trunk/Viaggio-Sogno-EJBClient/ejbModule/usermanagement/UserMgr.java
+++ trunk/Viaggio-Sogno-EJBClient/ejbModule/usermanagement/UserMgr.java
@@ -7,11 +7,17 @@ import usermanagement.dto.UserDTO;
 @Local
 public interface UserMgr {
 	
-	public void save(UserDTO user);
+	public void addCustomer(UserDTO user);
+
+	public void addEmployee(UserDTO user);
 	
 	public void update(UserDTO user);
 	
+	public void updateSelf(UserDTO user);
+	
 	public void unregister();
+	
+	public void remove(String email);
 	
 	public UserDTO getUserDTO();
 
