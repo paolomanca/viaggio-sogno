8beeaa2caca1ae55bced2fa22a56acc8dda875a8
paolo@manca.eu <paolo@manca.eu@81b2ac94-fbc2-da70-ebc2-4190521568d7>
2014-01-21 16:10:27 +0000

    - Added FinalPackage sketch classes

--- trunk/Viaggio-Sogno-EJBClient/ejbModule/dto/FinalPackageDTO.java
+++ trunk/Viaggio-Sogno-EJBClient/ejbModule/dto/FinalPackageDTO.java
@@ -1,5 +1,47 @@
 package dto;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import dto.ProductDTO;
+
 public class FinalPackageDTO {
+	
+	@NotEmpty
+	private int id;	
+	
+	@NotEmpty
+    private String name;
+    	
+	private List<ProductDTO> products;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+        return name;
+    }
+     
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	public List<ProductDTO> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<ProductDTO> products) {
+		this.products = products;
+	}
+
+
 
 }
